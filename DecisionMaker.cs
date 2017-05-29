19e1122d1aa33ad6f1dc34e714e363de0bdf16e6
Pavel Mezentsev <mezentsev@g5software.com>
2017-05-29 18:22:08 +0300

    algorightm?

--- DecisionMaker.cs
+++ DecisionMaker.cs
@@ -10,68 +10,43 @@ namespace TILab1WPF
     public class DecisionMaker
     {
         private UniverEntities _context;
-        public List<NormalizedCriterion> Criterions { get; set; } 
 
         public DecisionMaker(UniverEntities context)
         {
             _context = context;
-            Criterions = new List<NormalizedCriterion>();
-            NormalizeCriterions();
-        }
-
-        public void NormalizeCriterions()
-        {
-            int n = _context.Criteria.Count();
-            int total = 0;
-            for (int i = 1; i <= n; i++)
-            {
-                total += i;
-            }
-            List<int> weightList = _context.Criteria.Select(c => c.CWeight).Distinct().ToList();
-            decimal[] resultList = new decimal[n];
-            int diff = (n - weightList.Count) + 1;
-            for (int i = n, j = weightList.Max(); i > 0; i--,j--)
-            {
-                if (j < 1)
-                {
-                    int subtotal = 0;
-                    for (int k = 1; k <= i; i++)
-                    {
-                        subtotal += k;
-                    }
-                    decimal res = Convert.ToDecimal(subtotal)/diff;
-                    for (int k = 1; k <= i; k++)
-                    {
-                        resultList[k - 1] = res;
-                    }
-                    break;
-                }
-                resultList[i - 1] = i;
-            }
-            int index = 0;
-            foreach (Criterion criterion in _context.Criteria.OrderBy(c=>c.CWeight))
-            {
-                Criterions.Add(new NormalizedCriterion {Criterion = criterion, NormWeight = resultList[index]/total});
-            }
         }
 
         public void MakeDecision()
         {
-            var marksByCriterion = new Dictionary<NormalizedCriterion, List<Alternative>>();
-            foreach (NormalizedCriterion criterion in Criterions)
+            var result = new List<KeyValuePair<Result, float>>();
+
+            var criteriaToMulriplier = new Dictionary<Criterion, float>();
+            foreach (var c in _context.Criteria)
             {
-                var alternatives = new List<Alternative>();
-                var criterionMarks = _context.Marks.Where(m => m.CNum == criterion.Criterion.CNum).ToList();
-                bool isRange = criterion.Criterion.ScaleType == ScaleTypes.наименований.ToString();
-                var orderedMarks = isRange
-                    ? criterionMarks.OrderBy(m => m.MRange)
-                    : criterionMarks.OrderByDescending(m => m.NumMark);
-                foreach (Mark mark in orderedMarks)
-                {
-                    alternatives.AddRange(mark.Vectors.Select(v => v.Alternative));
-                }
-                marksByCriterion.Add(criterion, alternatives);
+                var marks = _context.Marks.Where(m => m.CNum == c.CNum).ToList();
+                float multiplier = 1 / marks.Sum(m => m.NumMark);
+
+                criteriaToMulriplier[c] = multiplier;
             }
+
+            foreach (var a in _context.Alternatives)
+            {
+                var vectors = _context.Vectors.Where(v => v.Alternative == a).ToList();
+
+                float ranking = 0.0f;
+                foreach (var v in vectors)
+                    ranking += v.Mark.NumMark * criteriaToMulriplier[v.Mark.Criterion];
+
+                var res = new Result();
+                res.Alternative = a;
+                res.LNum = 0;
+                
+
+                result.Add(new KeyValuePair<Result, float>(res, ranking));
+            }
+
+            var sortedResults = result.OrderBy(r => r.Value).Select(pair => pair.Key).ToList();
+            _context.Results.AddRange(sortedResults);
         }
     }
 }
