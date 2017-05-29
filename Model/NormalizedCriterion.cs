2d07a012dfc3e5b053cfb173a4f3a2f0f71a2d11
Pavel Mezentsev <mezentsev@g5software.com>
2017-05-29 18:24:04 +0300

    return missing file

---
+++ Model/NormalizedCriterion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TILab1WPF.Model
+{
+    public class NormalizedCriterion
+    {
+        public Criterion Criterion { get; set; }
+
+        public decimal NormWeight { get; set; }
+
+        
+    }
+}
